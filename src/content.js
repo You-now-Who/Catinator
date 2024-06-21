ca845d025403f9c3f8323ae688effae8dea6f525
Yash Yadav <yashyadav.sr@gmail.com>
2024-06-21 23:38:59 +0530

    feat: made cat replacer

---
+++ src/content.js
@@ -0,0 +1,3 @@
+document.querySelectorAll('img').forEach(function(img) {
+  img.src = 'https://cdn.pixabay.com/photo/2017/05/29/15/34/kitten-2354016_1280.jpg'; // Replace with your cat image URL
+});
